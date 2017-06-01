d2403803e7a9cc6ab960618296bede7645e3243d
Xuan Hu (Sean) <i+github@huxuan.org>
2017-06-01 11:29:13 +0800

    Fix uppercase extension problem. (#36)

--- Sample-WPF/Controls/FaceGroupingPage.xaml.cs
+++ Sample-WPF/Controls/FaceGroupingPage.xaml.cs
@@ -169,7 +169,7 @@ namespace Microsoft.ProjectOxford.Face.Controls
                 var imageList =
                     new ConcurrentBag<string>(
                         Directory.EnumerateFiles(dlg.SelectedPath, "*.*", SearchOption.AllDirectories)
-                            .Where(s => s.EndsWith(".jpg") || s.EndsWith(".png") || s.EndsWith(".bmp") || s.EndsWith(".gif")));
+                            .Where(s => s.ToLower().EndsWith(".jpg") || s.ToLower().EndsWith(".png") || s.ToLower().EndsWith(".bmp") || s.ToLower().EndsWith(".gif")));
 
                 string img;
                 while (imageList.TryTake(out img))
